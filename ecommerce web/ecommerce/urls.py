f4ae5cb6af7d15db904784a1329e5c5fc5cfb4b2
Ogunkoya Mayowa <108439268+MAYOWA107@users.noreply.github.com>
2024-09-21 10:23:47 +0100

    second commit

--- ecommerce web/ecommerce/urls.py
+++ ecommerce web/ecommerce/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ecommerce_app.urls')),
+    path('api/', include('ecommerce_app.urls')),
 ]
